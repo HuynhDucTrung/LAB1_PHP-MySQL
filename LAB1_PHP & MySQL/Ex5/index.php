63f1a0647bdc4228122a621918b79a007023293b
HuynhDucTrung <trunghd.124010123007@vtc.edu.vn>
2024-06-19 20:55:45 +0700

    Add files via upload

---
+++ LAB1_PHP & MySQL/Ex5/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print "<p>This is paragraph one and should be on top</p>";
+        print "<hr>";
+        print "<p>Hello<br>You delivered your assignment on time.<br>Thanks<br>Mahnaz</p>";
+        print "<hr>";
+        print "<p>This is paragraph two and should be at bottom</p>";
+?>
+</body>
+</html>
